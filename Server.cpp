e0187f0b3e67b7feac748b6e040267a5753ea341
Madennetic <8maden8@gmail.com>
2025-07-16 00:27:11 -0400

    changes made to try and parse the input, but i think i just messed everything up :P

--- Server.cpp
+++ Server.cpp
@@ -7,9 +7,134 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <thread>
+#include <vector>
+#include <regex>
 
 //https://redis.io/docs/latest/operate/oss_and_stack/install/archive/install-redis/install-redis-on-linux/
 // Redis documentation for starting and stopping
+
+// This is a debugging function for printing literal strings without \n etc...
+void print_literal(const std::string& str) {
+  for (char c : str) {
+    switch (c) {
+      case '\n': std::cout << "\\n"; break;
+      case '\r': std::cout << "\\r"; break;
+      case '\t': std::cout << "\\t"; break;
+      case '\\': std::cout << "\\\\"; break;
+      default: std::cout << c; break;
+    }
+  }
+  std::cout << std::endl;
+}
+
+// This function finds the first number in a string and returns it
+int primary_int_finder(const std::string& str) {
+  std::string num_str;
+
+  for (const char& c : str) {
+    if (std::isdigit(c)) {
+      num_str += c;
+    }
+    else if (!num_str.empty()) {
+      break;
+    }
+  }
+
+  return stoi(num_str);
+}
+
+std::vector<std::string> parse(std::string str) {
+
+  std::vector<std::string> parsed_commands;
+  int jump = 3 + str.find("\r\n");
+
+  if (str.substr(0, 1) == "*") {
+    int words = primary_int_finder(str);
+    for (int i = 0; i < words; i++) { //should be a while true in case data is longer than 1024
+      // this is assuming that these are all bulk strings with $
+      int current_str_length = primary_int_finder(str.substr(jump));
+      jump = (str.substr(jump)).find("\r\n") + jump + 2;
+      parsed_commands.push_back(str.substr(jump, current_str_length));
+      jump += current_str_length + 2;
+    }
+
+    return parsed_commands;
+
+  }
+  else {
+    std::cerr << "We don't know you yet" << std::endl;
+    return {};
+  }
+  return {};
+}
+
+// checks that the final characters of the data are the correct ones
+// This can still break which isn't good, if \r\n just happen to line up
+int resp_check(const std::string& str) {
+
+  if (str.size() >= 2 && str.substr(str.size() - 2, 2) == "\r\n") {
+    return 1;
+  }
+  return -1;
+}
+
+// This function is for handling multiple clients at once
+// This function will be a detached thread that runs
+// It receives the client, and loops a response until close/error
+// Then it closes the client and ends the function then and there
+void thread_socket(int client_fd) {
+
+  char buffer[1024];
+  std::string data;
+
+  while (true) {            // Outer loop
+    while (true) {          // Inner loop
+      // recv returns 0 at disconnect, -1 at error, and a number of bytes when proper
+      // If the number is higher than 1024 it loops
+      // It stores the received info in buffer
+      int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+      if (bytes == 0) {
+        std::cerr << "Client disconnected" << std::endl;
+        close(client_fd);
+        return;
+      }
+      else if (bytes < 0) {
+        std::cerr << "Client error" << std::endl;
+        close(client_fd);
+        return;
+      }
+      data.append(buffer, bytes);
+      if (resp_check(data) == 1) break;
+    }
+
+    bool echoed = false;
+    for (const std::string& command : parse(data)) {
+      print_literal(command);
+
+      if (echoed) {
+        std::string response = "+" + command + "\r\n";
+        send(client_fd, response.c_str(), response.size(), 0);
+        echoed = false;
+      }
+      else if (command == "PING") {
+        std::string response = "+PONG\r\n";
+        send(client_fd, response.c_str(), response.size(), 0);
+      }
+      else if (command == "ECHO") {
+        echoed = true;
+      }
+      else {
+        std::cout << "sent" << std::endl;
+        std::string response = "+PONG\r\n";
+        send(client_fd, response.c_str(), response.size(), 0);
+      }
+    }
+
+    data = "";
+
+  }
+}
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -48,42 +173,25 @@ int main(int argc, char **argv) {
   
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
-
-  // Uncomment this block to pass the first stage
-  // 
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "Failed to accept server";
-    return 1;
-  }
-
-  std::cout << "Client connected\n";
-
-  char buffer[1024];
-  int all_bytes = 0;
-
-  // Receives message from client, if larger than 1024 bytes it loops
+  // This loop accepts clients until the server is closed
+  // It creates a thread for each client and detaches them
   while (true) {
-    while (true) {
-        int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
-        // there is a bug here for if the client send a multiple of 1024
-        // then the loop breaks
-        all_bytes += bytes;
-        if (bytes < sizeof(buffer)) break;
+    std::cout << "Waiting for a client to connect...\n";
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    if (client_fd < 0) {
+      std::cerr << "Failed to accept server";
+      return 1;
     }
 
-    std::string response = "+PONG\r\n";
-    send(client_fd, response.c_str(), response.size(), 0);
+    std::thread current_thread(thread_socket, client_fd);
+
+    std::cout << "Client connected\n";
+
+    current_thread.detach();
 
   }
 
-  std::cout << all_bytes << std::endl;
-
-  close(client_fd);
   close(server_fd);
 
   return 0;
